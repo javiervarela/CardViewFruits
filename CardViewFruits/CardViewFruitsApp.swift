d1365e120801257c071aeeac64cc2aecc01ef1ee
Javier Angel Varela Cebey <gekito@gmail.com>
2023-12-05 18:49:07 +0100

    Initial Commit

---
+++ CardViewFruits/CardViewFruitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardViewFruitsApp.swift
+//  CardViewFruits
+//
+//  Created by Javier Angel Varela Cebey on 5/12/23.
+//
+
+import SwiftUI
+
+@main
+struct CardViewFruitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CardViewFruits/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CardViewFruits
+//
+//  Created by Javier Angel Varela Cebey on 5/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
